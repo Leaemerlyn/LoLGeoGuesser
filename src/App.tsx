aa4f0eb3833cb14e65057d3bd77f7462a570aa61
Leaemerlyn <59983824+Leaemerlyn@users.noreply.github.com>
2025-04-05 21:10:10 -0400

    install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import AatroxOriginal from './assets/Original_Skin/Aatrox_OriginalSkin_HD.jpg'
 import './App.css'
 import { RandomCrop } from './components/RandomCrop'
+import { Button } from './components/ui/button'
+import { Slider } from './components/ui/slider'
 
 function App() {
 
     return (
         <>
+          <Button variant="outline">Button</Button>
+          <Slider defaultValue={[33]} max={100} step={1} />
+
+          <>
             <RandomCrop src={AatroxOriginal} />
+          </>
+
         </>
     )
 }
