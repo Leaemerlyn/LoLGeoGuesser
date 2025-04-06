e7f616c4374b9f225ba0c479bb90159c8dcd46ca
Thomas Wrabetz <wrabetz.thomas@gmail.com>
2025-04-05 20:22:02 -0400

    Remove unused useState =.=

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import AatroxOriginal from './assets/Original_Skin/Aatrox_OriginalSkin_HD.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <img src={AatroxOriginal} style={{"height": "500px"}}/>
-    </>
-  )
+    return (
+        <>
+            <img src={AatroxOriginal} style={{ "height": "500px" }} />
+        </>
+    )
 }
 
 export default App
